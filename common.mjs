17c2c6870a4eabec2205e40cc1d30bb03ec512b9
Ahmad Hmedan <ahmadhmedann@gmail.com>
2026-02-24 14:58:16 +0000

    MisspelledWordArray has been created

--- common.mjs
+++ common.mjs
@@ -1,10 +1,26 @@
 import words from "./words.json" with { type: "json" };
 
-
-
 export const state = {
-    BasicEnglishWords: new Set (words),
-    userDictionary:[]
+  basicEnglishWords: new Set(words),
+  userDictionary: [],
+};
+
+function startsWithCapital(word) {
+  return word[0] === word[0]?.toUpperCase();
 }
 
-
+export function misspelledWordsList(wordsList) {
+  let list = [];
+  for (const word of wordsList) {
+    if (
+      !(
+        startsWithCapital(word) ||
+        state.basicEnglishWords.has(word.toLowerCase()) ||
+        state.userDictionary.includes(word.toLowerCase())
+      )
+    ) {
+      list.push(word);
+    }
+  }
+  return list;
+}
--- script.mjs
+++ script.mjs
@@ -1,13 +1,18 @@
 import * as Engine from "./common.mjs";
 
-window.onload = function() {
-   document.getElementById("check-spelling").addEventListener("click",inputHandler)
-}
+window.onload = function () {
+  document
+    .getElementById("check-spelling")
+    .addEventListener("click", inputHandler);
+};
 
-function inputHandler(event)
-{
-   const  textArea = document.getElementById("spell-input").value;
-   
-    const cleanWordsArr = textArea.replace(/-/g," ").replace(/[^\w\s]/g,"").split(/\s+/);
-     console.log(cleanWordsArr);
+function inputHandler(event) {
+  const textArea = document.getElementById("spell-input").value;
+
+  const cleanWordsArr = textArea
+    .replace(/-/g, " ")
+    .replace(/[^\w\s]/g, "")
+    .split(/\s+/);
+  const misspelledWord = Engine.misspelledWordsList(cleanWordsArr);
+  console.log(misspelledWord);
 }
