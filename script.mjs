4bc911f8452a299d11cc554c4355a8cfb5c84968
Ahmad Hmedan <ahmadhmedann@gmail.com>
2026-02-25 12:34:31 +0000

    display misspelled list

--- script.mjs
+++ script.mjs
@@ -13,6 +13,36 @@ function inputHandler(event) {
     .replace(/-/g, " ")
     .replace(/[^\w\s]/g, "")
     .split(/\s+/);
-  const misspelledWord = Engine.misspelledWordsList(cleanWordsArr);
-  console.log(misspelledWord);
+  const misspelledWords = Engine.misspelledWordsList(cleanWordsArr);
+  renderMisspellings(misspelledWords);
+}
+
+function renderMisspellings(misspelledWords) {
+  const root = document.getElementById("spell-results");
+  root.textContent = "";
+  if (misspelledWords.length === 0) return;
+
+  const warning = document.createElement("p");
+  warning.className = "warning";
+  warning.textContent = "Spelling mistakes found: "; //innerHTML later for highlight
+  root.appendChild(warning);
+  const list = document.createElement("ul");
+  list.id = "misspelled-list";
+  root.appendChild(list);
+
+  for (const word of misspelledWords) {
+    const li = document.createElement("li");
+    const strong = document.createElement("strong");
+    strong.textContent = word;
+    li.appendChild(strong);
+
+    const btn = document.createElement("button");
+    btn.textContent = " Add to the dictionary";
+    btn.type = "button";
+
+    li.append("  ");
+    li.appendChild(btn);
+
+    list.append(li);
+  }
 }
